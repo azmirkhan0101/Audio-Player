9989d931999a9dc25516c21b6bda691148b74ee4
Azmir Khan <azmir.azamkhan@gmail.com>
2023-04-04 17:21:56 +0600

    MY learned COMMIT.

--- app/src/main/java/com/azsoftech/audioplayer/PlayerControlActivity.java
+++ app/src/main/java/com/azsoftech/audioplayer/PlayerControlActivity.java
@@ -11,6 +11,8 @@ import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class PlayerControlActivity extends AppCompatActivity {
 
 
@@ -26,6 +28,7 @@ public class PlayerControlActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.player_control);
 
+        //###########################################
         iv_player_controlTop = findViewById(R.id.iv_player_controlTop);
         player_control_song_tittle = findViewById(R.id.player_control_song_tittle);
         iv_previous = findViewById(R.id.iv_previous);
@@ -33,6 +36,7 @@ public class PlayerControlActivity extends AppCompatActivity {
         iv_next = findViewById(R.id.iv_next);
         play_button = findViewById(R.id.play_button);
         seekBar = findViewById(R.id.seekBar);
+        //###########################################
 
         /*
         if( mediaPlayer==null ){
@@ -40,12 +44,14 @@ public class PlayerControlActivity extends AppCompatActivity {
         }
 
          */
-        mediaPlayer = new MediaPlayer();
+        //###########################################
+        mediaPlayer = MediaPlayer.create(this, R.raw.aradhona);
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
+        //###########################################
 
         player_control_song_tittle.setSelected(true);
 
+        //###########################################
         //CHANGE ACTIVITY
         iv_player_controlTop.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +60,7 @@ public class PlayerControlActivity extends AppCompatActivity {
                 overridePendingTransition( R.anim.slide_out_up02,R.anim.slide_in_up02 );
             }
         });
-
+        //###########################################
         //PREVIOUS SONG
         iv_previous.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,21 +75,23 @@ public class PlayerControlActivity extends AppCompatActivity {
 
             }
         });
+        //###########################################
         //PLAY SONG
         play_button.setEnabled(false);
         play_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if( !mediaPlayer.isPlaying() ){
-                    mediaPlayer.start();
-                    iv_play_pause.setImageResource(R.drawable.big_pause_white);
-                }else{
+                if( mediaPlayer.isPlaying() ){
                     mediaPlayer.pause();
                     iv_play_pause.setImageResource(R.drawable.big_player_white);
+                }else{
+                    mediaPlayer.start();
+                    iv_play_pause.setImageResource(R.drawable.big_pause_white);
                 }
             }
         });
+        //###########################################
         //NEXT SONG
         iv_next.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,6 +107,7 @@ public class PlayerControlActivity extends AppCompatActivity {
 
             }
         });
+        //###########################################
 
         /*
         int dur = mediaPlayer.getDuration();
@@ -106,9 +115,7 @@ public class PlayerControlActivity extends AppCompatActivity {
         int min = s/60;
         int es = s%60;
          */
-
-
-
+        //###########################################
         seekBar.setMax( mediaPlayer.getDuration() );
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -129,23 +136,67 @@ public class PlayerControlActivity extends AppCompatActivity {
 
             }
         });
-
-
+        //###########################################
+        //ON COMPLETION LISTENER
         mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
+                observer.stop();
                 iv_play_pause.setImageResource(R.drawable.big_player_white);
+                seekBar.setProgress( mp.getDuration() );
+                mediaPlayer.stop();
+                mediaPlayer.reset();
             }
         });
-
-
+        //###########################################
+        //BUFFERING UPDATE LISTENER
+        mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+            @Override
+            public void onBufferingUpdate(MediaPlayer mp, int percent) {
+                seekBar.setSecondaryProgress(percent);
+            }
+        });
+        //###########################################
+        //PREPARED LISTENER
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                play_button.setEnabled(true);
+            }
+        });
+        //###########################################
+        observer = new MediaObserver();
+        mediaPlayer.start();
+        new Thread( observer ).start();
 
     }
     //ONCREATE END
+    // ###########################################
 
+    private MediaObserver observer = null;
+    private  class MediaObserver implements Runnable{
+        private AtomicBoolean stop = new AtomicBoolean(false);
 
+        public void stop(){
+            stop.set(true);
+        }
 
+        @Override
+        public void run() {
+            while( !stop.get() ){
+                int playPercent = ( mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration() ) * 100;
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
 
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
+    }
+    //############################################
     //BACK PRESSED
     @Override
     public void onBackPressed() {
@@ -153,4 +204,5 @@ public class PlayerControlActivity extends AppCompatActivity {
         startActivity(new Intent(PlayerControlActivity.this,MainActivity.class));
         overridePendingTransition( R.anim.slide_out_up02,R.anim.slide_in_up02 );
     }
+    //###########################################
 }
